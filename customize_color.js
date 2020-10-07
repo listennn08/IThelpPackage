e5c33997f7c066207a9526f5304da1666d5087a0
listennn08 <52522402+listennn08@users.noreply.github.com>
2020-10-07 11:46:12 +0800

    Update customize_color.js

--- customize_color.js
+++ customize_color.js
@@ -1,64 +1,56 @@
 // ==UserScript==
 // @name         ithelp codeblock customize color
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://ithelp.ithome.com.tw/*
 // @grant        none
 // ==/UserScript==
-
 (function() {
     'use strict';
-    const classList = [
-        {
-            class: ['.hljs'],
-            color: '#eaeaea',
-            background: 'black',
-        },
-        {
-            class: ['.hljs-keyword', '.hljs-selector-tag'],
-            color: '#c397d8',
-        },
-        {
-            class: ['.hljs-title', '.hljs-section'],
-            color: '#7aa6da',
-        },
-        {
-            class: ['.hljs-string', '.hljs-symbol', '.hljs-bullet', '.hljs-addition'],
-            color: '#b9ca4a',
-        },
-        {
-            class: ['.hljs-attribute'],
-            color: '#e7c547',
-        },
-        {
-            class: ['.hljs-comment', '.hljs-quote'],
-            color: '#969896',
-        },
-        {
-            class: ['.hljs-variable', '.hljs-template-variable', '.hljs-tag', '.hljs-name',
-                     '.hljs-selector-id','.hljs-selector-class', '.hljs-regexp', '.hljs-deletion'],
-            color: '#d54e53',
-        },
-        {
-            class: ['.hljs-number', '.hljs-built_in', '.hljs-builtin-name', '.hljs-literal',
-                    '.hljs-type', '.hljs-params', '.hljs-meta', '.hljs-link'],
-            color: '#e78c45',
-        },
-        {
-            class: ['.markdown__style pre', '.editor-preview pre'],
-            background: 'black',
-        },
+    if (!localStorage.getItem('itHelp_css')) {
+        localStorage.setItem('itHelp_css', prompt('請輸入想使用的程式碼區塊主題!'));
+    }
+    const theme = localStorage.getItem('itHelp_css');
+    const cors = 'https://cors-anywhere.herokuapp.com/';
+    $.ajax({
+        url: `${cors}https://raw.githubusercontent.com/highlightjs/highlight.js/master/src/styles/${theme}.css`,
+        type: 'GET',
+        success: setStyle,
+        dataType: 'text'
+    });
+})();
+
+function setStyle(resp) {
+    const regList = [
+        /(?<class>.hljs-comment,\n.hljs-quote) {\s+color: (?<color>#\w+);\n}/,
+        /(?<class>.hljs-variable,\n.hljs-template-variable,\n.hljs-tag,\n.hljs-name,\n.hljs-selector-id,\n.hljs-selector-class,\n.hljs-regexp,\n.hljs-deletion) {\s+color: (?<color>#\w+);\n}/,
+        /(?<class>.hljs-number,\n.hljs-built_in,\n.hljs-builtin-name,\n.hljs-literal,\n.hljs-type,\n.hljs-params,\n.hljs-meta,\n.hljs-link) {\s+color: (?<color>#\w+);\n}/,
+        /(?<class>.hljs-attribute) {\s+color: (?<color>#\w+);\n}/,
+        /(?<class>.hljs-string,\n.hljs-symbol,\n.hljs-bullet,\n.hljs-addition) {\s+color: (?<color>#\w+);\n}/,
+        /(?<class>.hljs-title,\n.hljs-section) {\s+color: (?<color>#\w+);\n}/,
+        /(?<class>.hljs-keyword,\n.hljs-selector-tag) {\s+color: (?<color>#\w+);\n}/,
+        /(?<class>.hljs) {\s+.*\s+.*\s+background: (?<background>#\w+);\s+color: (?<color>#\w+);\s+.*\n}/
     ];
+    const classList = regList.map((el) => {
+        const { groups } = resp.match(el);
+        return {
+            class: groups.class.split(`,\n`),
+            color: groups.color,
+            background: groups.background,
+        }
+    })
     classList.forEach((item) => {
         item.class.forEach((c) => {
             document.querySelectorAll(c).forEach((ele) => {
                 const keys = Object.keys(item).filter((key) => key !== 'class');
                 keys.forEach((key) => {
-                    ele.style[key] = item[key];
+                    if (item[key]) {
+                        ele.style[key] = item[key];
+                    }
                 });
             });
         });
     });
-})();
+}
